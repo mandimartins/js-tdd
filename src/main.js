942d276e30902f67e602cfc54b2a131307fdd237
mandimartins <amandamartins23@gmail.com>
2020-01-25 13:43:29 -0300

    Adicionar check-coverage no prepush

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 /* eslint-disable consistent-return */
 const FizzBuzz = (num) => {
+  if (num === 0) return num;
   if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
   if (num % 3 === 0) return 'Fizz';
   if (num % 5 === 0) return 'Buzz';
